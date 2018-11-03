0e9562bacd2de05c80c8a60cbfd0fc0b3400b8fa
thechaos16 <thechaos16@gmail.com>
2018-11-04 00:41:01 +0900

    refactoring

---
+++ airsim_client.py
@@ -0,0 +1,57 @@
+
+import os
+import tempfile
+import pprint
+
+import airsim
+import numpy as np
+
+
+class AirsimClient:
+    def __init__(self, agent_type, interval=5):
+        if agent_type == 'drone':
+            self.client = airsim.MultirotorClient()
+            self.client.confirmConnection()
+            self.client.enableApiControl(True)
+            self.client.armDisarm(True)
+        else:
+            raise NotImplementedError()
+        self.interval = interval
+
+    def destroy(self):
+        self.client.enableApiControl(False)
+
+    def start(self):
+        self.client.takeoffAsync().join()
+
+    def get_state(self):
+        return self.client.getMultirotorState()
+
+    def get_images(self, image_type=''):
+        responses = self.client.simGetImages([
+            airsim.ImageRequest('0', airsim.ImageType.DepthVis),  # depth visualization image
+            airsim.ImageRequest('1', airsim.ImageType.DepthPerspective, True),  # depth in perspective projection
+            airsim.ImageRequest('1', airsim.ImageType.Scene),  # scene vision image in png format
+            airsim.ImageRequest('1', airsim.ImageType.Scene, False, False)
+            # scene vision image in uncompressed RGBA array
+        ])
+        return responses
+
+    def move(self, move_type, *args):
+        if move_type == 'position':
+            self._go_to_loc(*args)
+        elif move_type == 'velocity':
+            self._move_by_velocity(*args)
+        else:
+            raise NotImplementedError()
+
+    def _go_to_loc(self, mx, my, mz, velocity):
+        self.client.moveToPositionAsync(mx, my, mz, velocity).join()
+
+    def _move_by_velocity(self, vx, vy, vz):
+        self.client.moveByVelocityAsync(vx, vy, vz, self.interval).join()
+
+
+if __name__ == '__main__':
+    controller = AirsimClient('drone', 5)
+    controller.destroy()
--- random_fly.py
+++ random_fly.py
@@ -1,71 +1,27 @@
 
 import argparse
 
-import airsim
 import numpy as np
 
-
-def create_client(agent_type=''):
-    # connect to the AirSim simulator
-    client = airsim.MultirotorClient()
-    client.confirmConnection()
-    client.enableApiControl(True)
-    client.armDisarm(True)
-    return client
-
-
-def destroy_client(client):
-    # that's enough fun for now. let's quit cleanly
-    client.enableApiControl(False)
-
-
-def get_state(client):
-    # TODO: get type of client
-    return client.getMultirotorState()
-
-
-def start_client(client):
-    # TODO: get type of client
-    client.takeoffAsync().join()
-
-
-def move_client(client, move_type, *args):
-    if move_type == 'position':
-        client.moveToPositionAsync(*args).join()
-    elif move_type == 'velocity':
-        client.moveByVelocityAsync(*args).join()
-    else:
-        raise NotImplementedError()
-
-
-def get_images(client, image_type=''):
-    responses = client.simGetImages([
-        airsim.ImageRequest('0', airsim.ImageType.DepthVis),  # depth visualization image
-        airsim.ImageRequest('1', airsim.ImageType.DepthPerspective, True),  # depth in perspective projection
-        airsim.ImageRequest('1', airsim.ImageType.Scene),  # scene vision image in png format
-        airsim.ImageRequest('1', airsim.ImageType.Scene, False, False)  # scene vision image in uncompressed RGBA array
-    ])
-    return responses
+from airsim_client import AirsimClient
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument('--iteration', default=100, type=int)
-    args.add_argument('--duration', default=5, type=int)
+    args.add_argument('--interval', default=5, type=int)
     args.add_argument('--move-type', default='velocity', type=str)
     config = args.parse_args()
 
-    client = create_client()
-    start_client(client)
-    states = [get_state(client)]
+    client = AirsimClient('drone', config.interval)
+    client.start()
+    states = [client.get_state()]
 
     # NOTE: it looks like position is reversed coordination
     for _ in range(config.iteration):
-        vec = [np.random.uniform(-2, 2,), np.random.uniform(-2, 2,), np.random.uniform(-2, 2,), config.duration]
-        move_client(client, config.move_type, *vec)
-        cur_state = get_state(client)
+        vec = [np.random.uniform(-2, 2,), np.random.uniform(-2, 2,), np.random.uniform(-2, 2,)]
+        client.move('velocity', *vec)
+        cur_state = client.get_state()
         states.append(cur_state)
 
-    client.armDisarm(False)
-    client.reset()
-    destroy_client(client)
+    client.destroy()
