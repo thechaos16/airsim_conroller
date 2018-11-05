314ff1ef33c4335bcd118da743edb0907a3c61cd
thechaos16 <thechaos16@gmail.com>
2018-11-06 01:12:38 +0900

    add collision parser

--- random_fly.py
+++ random_fly.py
@@ -6,9 +6,25 @@ import numpy as np
 from airsim_client import AirsimClient
 
 
+def parse_state(state):
+    collision = state.collision
+    gps = state.gps_location
+    kinematic = state.kinematics_estimated
+    time_stamp = state.timestamp
+    return collision, gps, kinematic, time_stamp
+
+
+def detect_collision(state):
+    collision, _, _, _ = parse_state(state)
+    has_collide = collision.has_collided
+    position = collision.position
+    x, y, z = position.x_val, position.y_val, position.z_val
+    return has_collide, x, y, z
+
+
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument('--iteration', default=100, type=int)
+    args.add_argument('--iteration', default=50, type=int)
     args.add_argument('--interval', default=5, type=int)
     args.add_argument('--move-type', default='velocity', type=str)
     config = args.parse_args()
@@ -22,6 +38,9 @@ if __name__ == '__main__':
         vec = [np.random.uniform(-2, 2,), np.random.uniform(-2, 2,), np.random.uniform(-2, 2,)]
         client.move('velocity', *vec)
         cur_state = client.get_state()
+        collide, x, y, z = detect_collision(cur_state)
+        if collide:
+            print('Collision!!!!')
         states.append(cur_state)
 
     client.destroy()
